cb0f586db5bc35bae6a2a81424a55ffc9a78098b
Anuj Patel <anujpatel2899@gmail.com>
2023-09-03 14:48:12 +0200

    Added Routing and UseLocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
-function App() {
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import Add from "./pages/add/Add";
+import MyGigs from "./pages/myGigs/MyGigs";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./app.scss";
 
+const App = () => {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
